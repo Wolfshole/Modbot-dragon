f5262604f7dead8aafe87870956225852ef6cb40
Wulfy <168473440+Wulfy-g@users.noreply.github.com>
2026-01-27 20:40:06 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
-
 export default defineConfig({
-	// @ts-ignore
   output: 'server',
   adapter: vercel(),
 });
